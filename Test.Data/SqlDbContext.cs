e8dd51ce9d5879a051d2ee0008477db764296ec2
Stephen <ikefunastephen@gmail.com>
2022-02-03 14:23:22 +0100

    Add files via upload

---
+++ Test.Data/SqlDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test.Data.Entities;
+
+namespace Test.Data
+{
+    public class SqlDbContext : DbContext
+    {
+        public SqlDbContext(DbContextOptions<SqlDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Product>(p =>
+            {
+                p.HasKey(t => t.Id);
+                p.Property(t => t.Name).IsRequired();
+                p.Property(t => t.Description);
+            });
+        }
+    }
+}
