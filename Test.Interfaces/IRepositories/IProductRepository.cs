32b45de5bd26ba199c93e8db04eb6fc9bfce5348
Stephen <ikefunastephen@gmail.com>
2022-02-03 14:16:38 +0100

    Added data and interfaces

---
+++ Test.Interfaces/IRepositories/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Test.Data.Entities;
+
+namespace Test.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<Product> SaveProductAsync(Product product);
+        Task<List<Product>> GetAllProductsAsync();
+        Task<Product> GetProductAsync(string productId);
+        Task<Product> UpdateProductAsync(string productId, Product product);
+        Task DeleteProductAsync(string productId);
+    }
+}
---
+++ Test.Interfaces/IServices/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test.Data.Entities;
+
+namespace Test.Interfaces.IServices
+{
+    public interface IProductService
+    {
+        Task<Product> SaveProductAsync(Product product);
+        Task<List<Product>> GetAllProductsAsync();
+        Task<Product> GetProductAsync(string productId);
+        Task<Product> UpdateProductAsync(string productId, Product product);
+        Task DeleteProductAsync(string productId);
+    }
+}
